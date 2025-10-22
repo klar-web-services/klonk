40d5e5d5a3928eedf54e11602b56c4b5847b5526
FKWS <code@klar.ws>
2025-10-22 10:27:58 +0200

    Remove CLI

--- bunup.config.ts
+++ bunup.config.ts
@@ -4,8 +4,6 @@ export default defineConfig({
   entry: {
     // Main entry point
     'index': './src/index.ts',    
-    // CLI entry point
-    'cli': './src/cli.ts',
   },
   format: ['esm', 'cjs'],
   dts: true,
